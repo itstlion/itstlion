ec3c941080ce0976de42e0ec6c0a993b39f08a4d
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-27 20:22:05 +0100

    chore: [#1] :building_construction: Add core and shared modules

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule]
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreModule,
+    SharedModule
+  ]
 })
 export class AppModule {}
