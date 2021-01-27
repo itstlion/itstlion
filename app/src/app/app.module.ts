93002d551e9029a00ce0bdb752046be0260f8873
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-27 19:45:32 +0100

    chore: :heavy_plus_sign: Add Angular material v10.2

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,6 @@ import { AppComponent } from './app.component';
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule]
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule]
 })
 export class AppModule {}
