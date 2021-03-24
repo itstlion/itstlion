3b5b897310429fcd26681ee9c0b79cf4712a1910
Loïc Lamarque <loiclamarque777@gmail.com>
2021-03-24 08:33:53 +0100

    feat: [#1] :sparkles: Add releases UI for mobile

--- app/src/app/features/releases/releases.module.ts
+++ app/src/app/features/releases/releases.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@app/shared/shared.module';
 import { ReleasesComponent } from './pages';
 import { ReleasesRoutingModule } from './releases-routing.module';
 
 @NgModule({
   declarations: [ReleasesComponent],
-  imports: [CommonModule, ReleasesRoutingModule]
+  imports: [CommonModule, ReleasesRoutingModule, SharedModule]
 })
 export class ReleasesModule {}
--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  exports: [MatSnackBarModule]
+  exports: [MatCardModule, MatRippleModule, MatSnackBarModule]
 })
 export class SharedModule {}
