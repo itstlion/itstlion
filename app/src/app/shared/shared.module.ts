b884ec6d648f33be78cc298cefd066b694f2548e
Loïc Lamarque <43500570+LVMVRQUXL@users.noreply.github.com>
2021-05-05 13:44:35 +0200

    :sparkles: Feature #17 : Add spinner when loading data

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -15,6 +16,7 @@ import { HeaderComponent } from './components';
     MatBottomSheetModule,
     MatCardModule,
     MatListModule,
+    MatProgressSpinnerModule,
     MatRippleModule,
     MatSnackBarModule
   ],
