5aa35b0c1063a53acaea6d35c201ae04682ee10d
Loïc Lamarque <loiclamarque777@gmail.com>
2021-06-29 14:50:26 +0200

    style: [#83] :lipstick: Update UI of streaming links

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -18,6 +19,7 @@ import { FooterComponent, HeaderComponent } from './components';
     MatBottomSheetModule,
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatListModule,
     MatProgressSpinnerModule,
     MatRippleModule,
